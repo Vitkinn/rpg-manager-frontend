f98c92071d5d5dcf7b9f72a819419c58e39a4c01
Arthur Moser <arthur.moser@hotmail.com>
2022-12-03 12:09:10 -0300

    Personagens

--- pages/js/site.js
+++ pages/js/site.js
@@ -4,39 +4,40 @@
     }
 }
 //---------------------Validação------------------------
-// (function () {
-//     'use strict'
+(function () {
+  'use strict'
 
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     var forms = document.querySelectorAll('.needs-validation')
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
 
-//     // Loop over them and prevent submission
-//     Array.prototype.slice.call(forms)
-//         .forEach(function (form) {
-//             form.addEventListener('submit', function (event) {
-//                 if (!form.checkValidity()) {
-//                     event.preventDefault()
-//                     event.stopPropagation()
-//                 }
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+              if (!form.checkValidity()) {
+                  event.preventDefault()
+                  event.stopPropagation()
+              }
 
-//                 form.classList.add('was-validated')
-//             }, false)
-//         })
-// })()
+              form.classList.add('was-validated')
+          }, false)
+      })
+})()
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+coll[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+});
 }
+
 function prepareFrame(name) {
 
   document.getElementById("iframe").src="../"+name+"/"+name+".html"
